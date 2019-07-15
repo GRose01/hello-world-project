f6c1e8b33726af34bfc18b7ed3484eac897e9cf9
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-07-15 15:32:52 +0100

    webpack installed

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const DotEnv = require('dotenv-webpack')
+// const env = process.env.NODE_ENV !== 'production' ? (
+//   new DotEnv()
+// ) : (
+//   new webpack.EnvironmentPlugin({ ...process.env })
+// )
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
+      { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve('src'),
+    hot: true,
+    open: true,
+    port: 8000,
+    watchContentBase: true,
+    // historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+    // env
+  ]
+}
