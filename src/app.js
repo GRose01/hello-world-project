5c209b1a5f194e8d23d162d3a39f5b3b11573419
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-07-22 22:00:30 +0100

    home added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom' 
+import { BrowserRouter as Browser, Route, Switch } from 'react-router-dom'
+
+import './style.scss'
+
+import Home from './components/home'
+
 const App = () => {
-    return "Hellooooooo World"
+    return (
+        <Browser>
+            <div>
+                <Home />
+            </div>
+        </Browser>
+    )
 }
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
-  )
+)
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+       <div>
+           <h1>Welcome to the Hello World Project</h1>
+       </div>
+    )
+}
+
+export default Home
